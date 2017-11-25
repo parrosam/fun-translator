f628dfe83f6216889c405c23e5a3fb9216ad67b6
Samuel Parrotte <samuel.parrotte@gmail.com>
2017-11-25 14:29:19 -0800

    Update module format of translate

--- app/translator.js
+++ app/translator.js
@@ -1,44 +1,35 @@
 var rp = require('request-promise-native');
 var xmlParse = require('xml2js').parseString;
 
-var translator = function(){
-
+module.exports = translate;
+function translate(to, from, text) {    
     let OcpApimHeader = "Ocp-Apim-Subscription-Key";
     let key = process.env.TRANSLATOR_KEY;
-
+    
     let TranslatorBaseUrl = "https://api.microsofttranslator.com/V2/Http.svc/Translate";
-
-    var azureTranslate = function(to, from, text){        
-        var url = TranslatorBaseUrl + "?text=" + text + "&from=" + from + "&to=" + to + "&appid=";
-        
-        var headers = {};
-        headers[OcpApimHeader] = key;
-        var options = {
-            url: url,
-            headers: headers
-        };
-        
-        return rp(options)
-            .then(function(xml){                
-                return new Promise(function(resolve, reject){                    
-                    xmlParse(xml, function(err, result){
-                        if(err){
-                            reject(err);
-                        }   
-                        else{
-                            resolve(result.string._);
-                        }
-                    });
+    let url = TranslatorBaseUrl + "?text=" + text + "&from=" + from + "&to=" + to + "&appid=";
+    
+    var headers = {};
+    headers[OcpApimHeader] = key;
+    var options = {
+        url: url,
+        headers: headers
+    };
+    
+    return rp(options)
+        .then(function(xml){                
+            return new Promise(function(resolve, reject){                    
+                xmlParse(xml, function(err, result){
+                    if(err){
+                        reject(err);
+                    }   
+                    else{
+                        resolve(result.string._);
+                    }
                 });
-            })
-            .catch(function (err){
-                return err;
             });
-    };
-
-    return {
-        translate: azureTranslate
-    };
-}();
-
-module.exports = translator;
+        })
+        .catch(function (err){
+            return err;
+        });
+};
